4fe3d79702f48aed0b7ca6cdf6460803896a880d
Noah <dacrashbomber@gmail.com>
2023-10-28 14:57:57 -0400

    added immediateconversation object

---
+++ Assets/Scripts/DialogueStarter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueStarter : MonoBehaviour
+{
+    [SerializeField] private GameObject dialoguePanelPrefab;
+    [SerializeField] private TextAsset inkAsset;
+    [SerializeField] private string knot; 
+
+    private void Start()
+    {
+        GameObject panel = Instantiate(dialoguePanelPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        panel.transform.SetParent(FindObjectOfType<Canvas>().gameObject.transform);
+
+        RectTransform rectTransform = panel.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(0, 50, 0);
+        rectTransform.localScale = new Vector3(1f, 1f, 1);
+
+        if (panel.TryGetComponent(out DialoguePanel dialoguePanel))
+        {
+            dialoguePanel.StartConversation(inkAsset, knot);
+        }
+    }
+}
